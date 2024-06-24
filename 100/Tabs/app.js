b6ea2e36a727acce8231ff937517188151239331
jmabbas <mdabbas.it@gmail.com>
2024-06-24 17:44:12 +0530

    tabs

---
+++ 100/Tabs/app.js
@@ -0,0 +1,17 @@
+document.addEventListener("click", (e)=>{
+    if(e.target.matches(".tab-item a")) {
+        getTab(e.target);
+    }
+});
+
+function getTab(el) {
+    const active = document.querySelector(".active");
+    const visible = document.querySelector(".content-visible");
+    const tabContent = document.getElementById(el.href.split("#")[1]);
+
+    active.classList.toggle("active");
+    visible.classList.toggle("content-visible");
+
+    el.classList.add("active");
+    tabContent.classList.add("content-visible");
+}
